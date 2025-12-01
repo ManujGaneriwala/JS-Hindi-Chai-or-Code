5d75ac9a9da793acc12c98d32d77607af309bef1
ManujGaneriwala <111690218+ManujGaneriwala@users.noreply.github.com>
2025-12-01 17:05:06 +0000

    final changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=123456745;
+let accountEmail="manuj@123"
+var accountPassword=125848
+accountCity="jaipur"
+let accountState;
+
+//accountId=12345678765434;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+let name="Manuj"
+let age=23;
+let isMapped=false
+
+let marks=null
+
+console.log(typeof marks);
